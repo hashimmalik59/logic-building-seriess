6a742ef24bfde22dcf775c627d0c05e23808f0d1
hashimmalik59 <hm3630450@gmail.com>
2026-03-11 03:14:35 +0500

    Day6, Logic Building, Remove Duplicate elements from an array

---
+++ 06-day/logic.js
@@ -0,0 +1,9 @@
+// Day 6 | Remove Duplicate
+
+const nums = [9, 2, 9];
+
+for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] === nums[j]) console.log("Duplicate", nums[i]);
+    }
+}
