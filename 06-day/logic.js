97533e4267ecc16210894df50b690adc02a75279
hashimmalik59 <hm3630450@gmail.com>
2026-03-11 16:00:46 +0500

    Minor fix

--- 06-day/logic.js
+++ 06-day/logic.js
@@ -4,6 +4,8 @@ const nums = [9, 2, 9];
 
 for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-        if (nums[i] === nums[j]) console.log("Duplicate", nums[i]);
+        if (nums[i] === nums[j]) {
+            console.log("Duplicate", nums[i]);
+        }
     }
 }
