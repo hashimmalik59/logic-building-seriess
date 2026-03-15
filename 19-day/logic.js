94d39e319ecdda78dcae5f57f3bf42ca821c65d2
hashimmalik59 <hm3630450@gmail.com>
2026-03-15 05:49:36 +0500

    Day 19, Logic building, Removing a speicif item from an array of object

---
+++ 19-day/logic.js
@@ -0,0 +1,22 @@
+// Day 19 | Removing a speicif item from an array of object
+
+const mobiles = [
+    { id: 101, model: "Samsung S24", price: 250000, stock: 5 },
+    { id: 102, model: "iPhone 15", price: 180000, stock: 1 },
+    { id: 103, model: "Google Pixel 8", price: 190000, stock: 4 },
+    { id: 104, model: "Nothing Phone 2", price: 120000, stock: 1 },
+    { id: 105, model: "Xiaomi 14", price: 150000, stock: 10 }
+];
+
+const newMobiles = [];
+
+for (let i = 0; i < mobiles.length; i++) {
+    if (mobiles[i].id !== 102) {
+        newMobiles.push(mobiles[i]);
+    }
+}
+
+console.log("OLD MOBILES");
+console.log(mobiles);
+console.log("NEW MOBILES");
+console.log(newMobiles);
